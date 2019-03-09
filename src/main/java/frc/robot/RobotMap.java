3984f3d66ba933dcbbc1acb7ebe6f6d4c64aba87
James Heinemann <jheinemann20@setoncatholic.org>
2019-03-09 14:22:15 -0700

    added RobotMap.java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,6 +7,18 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -23,4 +35,50 @@ public class RobotMap {
   // number and the module. For example you with a rangefinder:
   // public static int rangefinderPort = 1;
   // public static int rangefinderModule = 1;
+
+  public CANSparkMax
+      fL = new CANSparkMax(3, MotorType.kBrushless),
+      fR = new CANSparkMax(5, MotorType.kBrushless),
+      rL = new CANSparkMax(2, MotorType.kBrushless),
+      rR = new CANSparkMax(4, MotorType.kBrushless);
+
+  public VictorSPX
+      lifter1 = new VictorSPX(10),
+      lifter2 = new VictorSPX(9),
+      elevator = new VictorSPX(11),
+      leftHerder = new VictorSPX(0),
+      rightHerder = new VictorSPX(0),
+      ballHolder = new VictorSPX(0),
+      liftDrive = new VictorSPX(12);
+
+  public Joystick
+      myJoy = new Joystick(0),
+      myJoy2 = new Joystick(1);
+
+  public MecanumDrive
+      myDrive = new MecanumDrive(fL, fR, rL, rR);
+
+  public DoubleSolenoid
+      shifter = new DoubleSolenoid(0, 1);
+
+  public boolean
+      driveToggle = false,
+      shiftToggle = true,
+      debounce = false,
+      debounce_two = false;
+
+  public double
+      deadband = 0.05,
+      joyX, // modified in drive subsystem
+      joyY, // modified in drive subsystem
+      joyZ; // modified in drive subsystem
+
+  public double addDeadband(double x) {
+    if (x >= deadband)
+      return x;
+    else if (x <= -deadband)
+      return x;
+    else
+      return 0;
+  }
 }
