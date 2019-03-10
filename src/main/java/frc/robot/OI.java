b61002a26a10c05ed35325f873c6d771b4e2a257
Robotics Laptop 2 <cdavis19@setoncatholic.org>
2019-03-09 17:18:50 -0700

    Changes
    Added Herder Command(Base for all herder auto movement), Mecanum Command (Base for all auto drive movement), LineFollow, Moved Joystick and shifter from DriveSub to OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,9 +1,12 @@
+
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
+// RobotBuilder Version: 2.0
+//
 
 package frc.robot;
 
@@ -11,6 +14,13 @@ package frc.robot;
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
+import frc.robot.commands.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.*;
+
 public class OI {
   //// CREATING BUTTONS
   // One type of button is a joystick button which is any button on a
@@ -39,4 +49,67 @@ public class OI {
   // Start the command when the button is released and let it run the command
   // until it is finished as determined by it's isFinished method.
   // button.whenReleased(new ExampleCommand());
+
+  //Instance Variables
+  public Joystick
+    driveJoystick = new Joystick(0),
+    elevatorJoystick = new Joystick(1);
+
+  public static DoubleSolenoid
+    shifter = new DoubleSolenoid(0, 1);
+
+  private static DigitalInput rightEye;
+  private static DigitalInput midEye;
+  private static DigitalInput leftEye;
+
+  private static double xValue;
+  private static double yValue;
+  private static double twistValue;
+
+	// OI constructor method
+  public OI() 
+  {  
+
+  }
+
+  public double getXValue()
+  {
+    return xValue;
+  }
+
+  public double getYValue()
+  {
+    return yValue;
+  }
+
+  public double getTwistValue()
+  {
+    return twistValue;
+  }
+
+  public Joystick getDriveJoystick()
+  {
+    return driveJoystick;
+  }
+
+  public Joystick getElevatorstick()
+  {
+    return elevatorJoystick;
+  }
+
+  public boolean getLeftEye() 
+  {
+    return leftEye.get();
+  }
+
+  public boolean getRightEye()
+  {
+    return rightEye.get();
+  }
+
+  public boolean getMidEye()
+  {
+    return midEye.get();
+  }
+
 }
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,63 +1,83 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+    // public static int rangefinderPort = 1;
+    // public static int rangefinderModule = 1;
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
-
-  public static int // drivechannels
-      frontLeftChannel = 3,
-      frontRightChannel = 5,
-      rearLeftChannel = 2,
-      rearRightChannel = 4;
-
-  public static int // motor channels
-      lifter1Channel = 10,
-      lifter2Channel = 9,
-      elevatorChannel = 11,
-      leftHerderChannel = 0,
-      rightHerderChannel = 0,
-      ballHolderChannel = 0,
-      liftDriveChannel = 12;
-
-  public static int // joystick channels
-      myJoyChannel = 0,
-      myJoy2Channel = 1;
-
-  public static int[] // solenoid channels
-      shifterChannel = new int[]{0, 1};
-
-  public static double // deadband channels
-      deadbandChannel = 0.05;
-}
+    public static MecanumDrive mecanumDrive;
+  
+    public CANSparkMax
+        fL = new CANSparkMax(3, MotorType.kBrushless),
+        fR = new CANSparkMax(5, MotorType.kBrushless),
+        rL = new CANSparkMax(2, MotorType.kBrushless),
+        rR = new CANSparkMax(4, MotorType.kBrushless);
+  
+    public VictorSPX
+        lifter1 = new VictorSPX(10),
+        lifter2 = new VictorSPX(9),
+        elevator = new VictorSPX(11),
+        leftHerder = new VictorSPX(0),
+        rightHerder = new VictorSPX(0),
+        ballHolder = new VictorSPX(0),
+        liftDrive = new VictorSPX(12);
+  
+    public Joystick
+        myJoy = new Joystick(0),
+        myJoy2 = new Joystick(1);
+  
+    public MecanumDrive
+        myDrive = new MecanumDrive(fL, fR, rL, rR);
+  
+    public boolean
+        driveToggle = false,
+        shiftToggle = true,
+        debounce = false,
+        debounce_two = false;
+  
+    public double
+        deadband = 0.05,
+        joyX, // modified in drive subsystem
+        joyY, // modified in drive subsystem
+        joyZ; // modified in drive subsystem
+  
+    public double addDeadband(double x) {
+      if (x >= deadband)
+        return x;
+      else if (x <= -deadband)
+        return x;
+      else
+        return 0;
+    }
+    public static int // drivechannels
+        frontLeftChannel = 3,
+        frontRightChannel = 5,
+        rearLeftChannel = 2,
+        rearRightChannel = 4;
+  
+    public static int // motor channels
+        lifter1Channel = 10,
+        lifter2Channel = 9,
+        elevatorChannel = 11,
+        leftHerderChannel = 0,
+        rightHerderChannel = 0,
+        ballHolderChannel = 0,
+        liftDriveChannel = 12;
+  
+    public static int // joystick channels
+        myJoyChannel = 0,
+        myJoy2Channel = 1;
+  
+    public static int[] // solenoid channels
+        shifterChannel = new int[]{0, 1};
+  
+    public static double // deadband channels
+        deadbandChannel = 0.05;  
+  }
